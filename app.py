243bef7c477632bf8998825fec97e64961a5ea31
albiaju <albi.aju@btech.christuniversity.in>
2025-05-14 13:30:23 +0530

    add

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 import os
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
+load_dotenv()
 
+def run_chat():
+    chat = ChatOpenAI(
+        temperature=0.7,
+        model="gpt-4o",
+        api_key=os.getenv("OPENAI_API_KEY")
+    )
+
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["exit", "quit"]:
+            break
+        response = chat.invoke([HumanMessage(content=user_input)])
+        print("Bot:", response.content)
+
+run_chat()
